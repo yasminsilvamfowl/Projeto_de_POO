eb05dc91ec6c4e486c0f0b93f5b6f1f26ce592f2
Luis Ferlim Lima <101893830+Luisferlim@users.noreply.github.com>
2023-06-19 11:21:34 -0300

    Add files via upload

---
+++ Trabalho_de_POO/Estudante.java
@@ -0,0 +1,30 @@
+package Estudantes;
+
+public class Estudante {
+	
+	private String nome; //nome publico
+	private float nota; //nota do Enem
+	
+	public static void main(String[] args ) {
+	}
+	
+	public Estudante(String nome, float nota) {
+		this.nome = nome;
+		this.nota = nota;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public float getNota() {
+		return this.nota;
+	}
+	
+	public int compararEstudante (Estudante outroEstudante) {
+		return Float.compare(outroEstudante.getNota(), this.nota);
+		//0: if f1 is numerically equal to f2.
+		//Negative value: if f1 is numerically less than f2.
+		//Positive value: if f1 is numerically greater than f2.
+	}
+}
